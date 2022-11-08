991251c28cab49545a196ffd94424eb6ca164731
Eason Wong <wangyatsing@gmail.com>
2022-11-08 05:49:44 +0100

    UPDATE

--- reveal-nav-bar-javascript.js
+++ reveal-nav-bar-javascript.js
@@ -1,10 +1,33 @@
-var x = document.getElementById("guide");
+document.getElementById("guide").style.display = "none";
 
-x.style.display = "none";
+document.getElementById("guide-button").addEventListener("click", () => {
+  document.getElementById("guide").style.display = "block";
+});
 
-function myFunction() {
-  var x = document.getElementById("guide");
-  x.style.display = "block";
-}
+document.addEventListener('load', () => {
+  console.log("loaded");
+  window.dispatchEvent(new Event('resize'));
+});
 
-document.getElementById("guide-button").setAttribute("onclick", 'myFunction()');
+document.addEventListener('readystatechange', event => {
+  console.log("document.readyState: ", document.readyState);
+  switch (document.readyState) {
+    case "loading":
+      console.log("document.readyState: ", document.readyState,
+        `- The document is still loading.`
+      );
+      break;
+    case "interactive":
+      console.log("document.readyState: ", document.readyState,
+        `- The document has finished loading DOM. `,
+        `- "DOMContentLoaded" event`
+      );
+      break;
+    case "complete":
+      console.log("document.readyState: ", document.readyState,
+        `- The page DOM with Sub-resources are now fully loaded. `,
+        `- "load" event`
+      );
+      break;
+  }
+});
