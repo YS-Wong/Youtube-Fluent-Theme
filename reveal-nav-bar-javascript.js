70dd49cc917388ef9c76ef68cc9eb23caa341ef7
Eason Wong <eason.wong.yisum@gmail.com>
2022-11-08 05:01:30 +0100

    Update reveal-nav-bar-javascript.js

--- reveal-nav-bar-javascript.js
+++ reveal-nav-bar-javascript.js
@@ -1,4 +1,4 @@
 document.getElementById("guide").style.display = "none";
 document.getElementById("guide-button").addEventListener("click", () => {
-  document.getElementById("guide").style.display = "block";
+    document.getElementById("guide").style.display = "block";
 });
